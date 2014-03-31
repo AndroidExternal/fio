321fc5a7bda19c4d27b669ee1b21d57338adc2a1
chenh <hchen@redhat.com>
2014-03-31 11:32:29 -0400

    fix man page; fix read work

--- options.c
+++ options.c
@@ -1533,6 +1533,12 @@ struct fio_option fio_options[FIO_MAX_OPTS] = {
 			    .help = "fallocate() file based engine",
 			  },
 #endif
+#ifdef CONFIG_GFAPI
+			  { .ival = "gfapi",
+			    .help = "Glusterfs libgfapi based engine"
+			  },
+#endif
+
 			  { .ival = "external",
 			    .help = "Load external engine (append name)",
 			  },
