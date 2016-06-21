873e189d072097809dfb8bbee166396a0236c026
Joe Maples <joe@frap129.org>
2016-06-21 22:22:43 +0000

    Remove old bug fix, use ARM mode
    Signed-off-by: Joe Maples <joe@frap129.org>

--- Android.mk
+++ Android.mk
@@ -17,10 +17,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 SRCDIR := $(LOCAL_PATH)
-ifeq ($(TARGET_ARCH),arm)
-# b/17167262, Cannot link gettime.c, undefined __aeabi_read_tp, when compiled with -fpie.
-LOCAL_CLANG := false
-endif
+
+LOCAL_ARM_MODE := arm
 
 LOCAL_CFLAGS_32 += -DBITS_PER_LONG=32 -DCONFIG_64BIT
 LOCAL_CFLAGS_64 += -DBITS_PER_LONG=64 -DCONFIG_32BIT
